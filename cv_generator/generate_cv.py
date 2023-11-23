cc416bfb794d958a3bc1d62defbcaf286b38f9c5
wejpurvis <williampurvis00@gmail.com>
2023-11-23 05:58:11 +0000

    model in .py

--- cv_generator/generate_cv.py
+++ cv_generator/generate_cv.py
@@ -1,14 +1,19 @@
 from openai import OpenAI
+from dotenv import load_dotenv
 
+def generate_cover_letter(cv, job_desc):
+    client = OpenAI()
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant. Create a cover letter based on the following CV and job description."},
+            {"role": "user", "content": f"CV:\n{cv}\n\nJob Description:\n{job_desc}"}
+        ]
+    )
 
+    cover_letter = response.choices[0].message.content
+    return cover_letter
 
-client = OpenAI()
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
-
-print(completion.choices[0].message)
+def save_cover_letter(cover_letter, name = 'cover_letter.txt'):
+    with open(name, "w") as f:
+        f.write(cover_letter)
